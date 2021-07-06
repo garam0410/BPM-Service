cff828961843a20d70ab2678254d201fec5779b2
Garam Kim <0410garam@naver.com>
2021-07-06 16:09:45 +0900

    영화 순위 서버 연동

--- Application/mobile/src/main/java/com/example/bpm_service/connection/MovieInformationServer.java
+++ Application/mobile/src/main/java/com/example/bpm_service/connection/MovieInformationServer.java
@@ -1,15 +1,20 @@
 package com.example.bpm_service.connection;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.concurrent.ExecutionException;
 
 public class MovieInformationServer {
     private String url = "";
+    private String data = "";
 
     public MovieInformationServer(String IP){
         this.url = IP;
     }
 
-    // 검색
+    //검색
     public String search(String query){
         try {
             url += "8081/searchmovie?query="+query;
@@ -20,6 +25,37 @@ public class MovieInformationServer {
         } catch (InterruptedException e) {
             e.printStackTrace();
             return "InterruptedError";
+        }
+    }
+
+    // 영화 순위
+    public String hotMovieRank(){
+        try {
+            url += "8081/hotmovierank";
+            return new GetTask().execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return "ExecutionError";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "InterruptedError";
+        }
+    }
+
+    // 상세 정보
+    public JSONObject getInfo(String title){
+        try{
+            url += "8081/movieinfo?title="+title;
+            data = new GetTask().execute(url).get();
+
+            JSONArray json = new JSONArray(data);
+            JSONObject obj = (JSONObject)json.get(0);
+
+            return obj;
+
+        }catch(Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }

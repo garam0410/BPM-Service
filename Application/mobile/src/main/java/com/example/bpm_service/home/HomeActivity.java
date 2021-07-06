02356acdabe7639263d41480501f385379973833
Garam Kim <0410garam@naver.com>
2021-07-06 16:09:20 +0900

    홈화면 구성

--- Application/mobile/src/main/java/com/example/bpm_service/home/HomeActivity.java
+++ Application/mobile/src/main/java/com/example/bpm_service/home/HomeActivity.java
@@ -5,23 +5,101 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.bpm_service.MainActivity;
 import com.example.bpm_service.R;
+import com.example.bpm_service.connection.MovieInformationServer;
+import com.example.bpm_service.minfo.MovieListAdapter;
+import com.example.bpm_service.minfo.MyListDecoration;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class HomeActivity extends Fragment {
+
+    private RecyclerView movieRankList, userRankList;
+    private MovieListAdapter movieListAdapter_rank;
+    private MovieListAdapter movieListAdapter_user;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
         View view = inflater.inflate(R.layout.activity_home, container, false);
+
+        String IP = getResources().getString(R.string.IP);
+        //String IP = "http://61.245.226.112:";
+
+        movieRankList = view.findViewById(R.id.hotMovieRank);
+        userRankList = view.findViewById(R.id.userMovieRank);
 
         ActionBar actionbar = ((MainActivity)getActivity()).getSupportActionBar();
         actionbar.hide();
 
+        MovieInformationServer movieInformationServer = new MovieInformationServer(IP);
+
+        String hotData = movieInformationServer.hotMovieRank();
+        //String userData = movieInformationServer.userMovieRank();
+
+        init(movieRankList,movieListAdapter_rank, hotData);
+        init(userRankList,movieListAdapter_user, hotData);
+
         return view;
     }
+
+    private void init(RecyclerView recyclerView, MovieListAdapter movieListAdapter, String data) {
+
+        ArrayList<String> titleList = new ArrayList<>();
+        ArrayList<String> imageList = new ArrayList<>();
+
+        try{
+            JSONArray json = new JSONArray(data);
+            for(int i = 0; i<json.length(); i++){
+                JSONObject obj = (JSONObject)json.get(i);
+
+                String title = obj.getString("title");
+                String image = obj.getString("image");
+
+                titleList.add(title);
+                imageList.add(image);
+            }
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<String> itemList = new ArrayList<>();
+        itemList.add("1");
+        itemList.add("2");
+        itemList.add("3");
+        itemList.add("4");
+        itemList.add("5");
+
+        movieListAdapter = new MovieListAdapter(getActivity(), itemList, titleList,imageList, onClickItem);
+        recyclerView.setAdapter(movieListAdapter);
+
+        MyListDecoration decoration = new MyListDecoration();
+        recyclerView.addItemDecoration(decoration);
+    }
+
+
+    private View.OnClickListener onClickItem = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String str = (String) v.getTag();
+            Toast.makeText(getActivity(), str, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
---
+++ Application/mobile/src/main/java/com/example/bpm_service/minfo/MovieListAdapter.java
@@ -0,0 +1,79 @@
+package com.example.bpm_service.minfo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.bpm_service.R;
+
+import java.util.ArrayList;
+
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.ViewHolder>{
+
+    private ArrayList<String> itemList;
+    private Context context;
+    private View.OnClickListener onClickItem;
+
+    private ArrayList<String> titleList;
+    private ArrayList<String> imageList;
+
+    public MovieListAdapter(Context context, ArrayList<String> itemList, ArrayList<String> titleList, ArrayList<String> imageList ,View.OnClickListener onClickItem) {
+        this.context = context;
+        this.itemList = itemList;
+        this.titleList = titleList;
+        this.imageList = imageList;
+        this.onClickItem = onClickItem;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        // context 와 parent.getContext() 는 같다.
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.movierank_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        String item = itemList.get(position);
+        String title = titleList.get(position);
+        String image = imageList.get(position);
+
+        holder.textview.setText(item);
+        holder.item.setTag(title);
+        Glide.with(holder.itemView.getContext()).load(image).into(holder.imageView);
+        holder.item.setOnClickListener(onClickItem);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public LinearLayout item;
+        public TextView textview;
+        public ImageView imageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            item = itemView.findViewById(R.id.item);
+            textview = itemView.findViewById(R.id.rank);
+            imageView = itemView.findViewById(R.id.poster);
+        }
+    }
+}

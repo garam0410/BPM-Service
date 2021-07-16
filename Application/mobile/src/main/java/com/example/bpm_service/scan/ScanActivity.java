031fd508adc87619b1630270c37f9cf9b8e35def
Garam Kim <0410garam@naver.com>
2021-07-16 14:45:17 +0900

    영화표 인식해서 데이터 추출 구현

--- Application/mobile/src/main/java/com/example/bpm_service/scan/ScanActivity.java
+++ Application/mobile/src/main/java/com/example/bpm_service/scan/ScanActivity.java
@@ -2,28 +2,98 @@ package com.example.bpm_service.scan;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
-import android.app.Notification;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.bpm_service.R;
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ScanActivity extends AppCompatActivity {
 
     private Button applyButton, cancelButton;
+    private ImageButton qrButton;
+
+    // true  : Camera On  : 카메라로 직접 찍어 문자 인식
+    // false : Camera Off : 샘플이미지를 로드하여 문자 인식
+    private boolean CameraOnOffFlag = true;
+
+    private TessBaseAPI m_Tess; //Tess API reference
+    private ProgressCircleDialog m_objProgressCircle = null; // 원형 프로그레스바
+    private MessageHandler m_messageHandler;
+
+    private Context mContext;
+    private String mDataPath = ""; //언어데이터가 있는 경로
+    private String mCurrentPhotoPath; // 사진 경로
+    private final String[] mLanguageList = {"eng","kor"}; // 언어
+    // View
+    private TextView m_ocrTextView; // 결과 변환 텍스트
+    private Bitmap image; //사용되는 이미지
+    private boolean ProgressFlag = false; // 프로그레스바 상태 플래그
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        mContext = this;
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("QR 영화 등록");
 
+        // 촬영 시작
+        qrButton = (ImageButton) findViewById(R.id.qrStart);
+
+        //결과 텍스트
+        m_ocrTextView = (TextView) findViewById(R.id.textView);
+
         cancelButton = (Button) findViewById(R.id.cancel_button);
         applyButton = (Button) findViewById(R.id.apply_button);
+
+        m_objProgressCircle = new ProgressCircleDialog(this);
+        m_messageHandler = new MessageHandler();
+
+        if(CameraOnOffFlag)
+        {
+            PermissionCheck();
+            Tesseract();
+        }
+        else
+        {
+            //이미지 디코딩을 위한 초기화
+            image = BitmapFactory.decodeResource(getResources(), R.drawable.sampledata); //샘플이미지파일
+            Test();
+        }
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +101,326 @@ public class ScanActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(CameraOnOffFlag)
+                {
+                    dispatchTakePictureIntent();
+                }
+                else
+                {
+                    processImage();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case ConstantDefine.PERMISSION_CODE:
+                Toast.makeText(this, "권한이 허용되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+            case ConstantDefine.ACT_TAKE_PIC:
+                //카메라로 찍은 사진을 받는다.
+                if ((resultCode == RESULT_OK) ) {
+
+                    try {
+                        File file = new File(mCurrentPhotoPath);
+                        Bitmap rotatedBitmap = null;
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),
+                                FileProvider.getUriForFile(ScanActivity.this,
+                                        getApplicationContext().getPackageName() + ".fileprovider", file));
+
+                        // 회전된 사진을 원래대로 돌려 표시한다.
+                        if (bitmap != null) {
+                            ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
+                            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                                    ExifInterface.ORIENTATION_UNDEFINED);
+                            switch (orientation) {
+
+                                case ExifInterface.ORIENTATION_ROTATE_90:
+                                    rotatedBitmap = rotateImage(bitmap, 90);
+                                    break;
+
+                                case ExifInterface.ORIENTATION_ROTATE_180:
+                                    rotatedBitmap = rotateImage(bitmap, 180);
+                                    break;
+
+                                case ExifInterface.ORIENTATION_ROTATE_270:
+                                    rotatedBitmap = rotateImage(bitmap, 270);
+                                    break;
+
+                                case ExifInterface.ORIENTATION_NORMAL:
+                                default:
+                                    rotatedBitmap = bitmap;
+                            }
+                            OCRThread ocrThread = new OCRThread(rotatedBitmap);
+                            ocrThread.setDaemon(true);
+                            ocrThread.start();
+                            qrButton.setImageBitmap(rotatedBitmap);// 카메라로 찍은 사진을 뷰에 표시한다.
+                            m_ocrTextView.setText(getResources().getString(R.string.LoadingMessage)); //인식된텍스트 표시
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,  int[] grantResult) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResult);
+        if (requestCode == 0) {
+
+        } else {
+
+        }
+    }
+
+    // 이미지를 원본과 같게 회전시킨다.
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+    public void PermissionCheck() {
+        /**
+         * 6.0 마시멜로우 이상일 경우에는 권한 체크후 권한을 요청한다.
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED &&
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED &&
+                    checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                // 권한 없음
+                ActivityCompat.requestPermissions(ScanActivity.this,
+                        new String[]{Manifest.permission.CAMERA,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        ConstantDefine.PERMISSION_CODE);
+            } else {
+                // 권한 있음
+            }
+        }
+    }
+
+
+    public void Tesseract() {
+        //언어파일 경로
+        mDataPath = getFilesDir() + "/tesseract/";
+
+        //트레이닝데이터가 카피되어 있는지 체크
+        String lang = "";
+        for (String Language : mLanguageList) {
+            checkFile(new File(mDataPath + "tessdata/"), Language);
+            lang += Language + "+";
+        }
+        m_Tess = new TessBaseAPI();
+        m_Tess.init(mDataPath, lang);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    /**
+     * 기본카메라앱을 실행 시킨다.
+     */
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // 사진파일을 생성한다.
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // 사진파일이 정상적으로 생성되었을때
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        this.getApplicationContext().getPackageName()+".fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, ConstantDefine.ACT_TAKE_PIC);
+            }
+        }
+    }
+
+    //copy file to device
+    private void copyFiles(String Language) {
+        try {
+            String filepath = mDataPath + "/tessdata/" + Language + ".traineddata";
+            AssetManager assetManager = getAssets();
+            InputStream instream = assetManager.open("tessdata/"+Language+".traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //check file on the device
+    private void checkFile(File dir, String Language) {
+        //디렉토리가 없으면 디렉토리를 만들고 그후에 파일을 카피
+        if (!dir.exists() && dir.mkdirs()) {
+            copyFiles(Language);
+        }
+        //디렉토리가 있지만 파일이 없으면 파일카피 진행
+        if (dir.exists()) {
+            String datafilepath = mDataPath + "tessdata/" + Language + ".traineddata";
+            File datafile = new File(datafilepath);
+            if (!datafile.exists()) {
+                copyFiles(Language);
+            }
+        }
+    }
+
+    //region Thread
+    public class OCRThread extends Thread
+    {
+        private Bitmap rotatedImage;
+        OCRThread(Bitmap rotatedImage)
+        {
+            this.rotatedImage = rotatedImage;
+            if(!ProgressFlag)
+                m_objProgressCircle = ProgressCircleDialog.show(mContext, "", "", true);
+            ProgressFlag = true;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            // 사진의 글자를 인식해서 옮긴다
+            String OCRresult = null;
+            m_Tess.setImage(rotatedImage);
+            OCRresult = m_Tess.getUTF8Text();
+
+            Message message = Message.obtain();
+            message.what = ConstantDefine.RESULT_OCR;
+            message.obj = OCRresult;
+            m_messageHandler.sendMessage(message);
+
+        }
+    }
+    //endregion
+
+    //region Handler
+    public class MessageHandler extends Handler
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            switch (msg.what)
+            {
+                case ConstantDefine.RESULT_OCR:
+                    TextView OCRTextView = findViewById(R.id.textView);
+                    OCRTextView.setText(String.valueOf(msg.obj)); //텍스트 변경
+
+                    // 원형 프로그레스바 종료
+                    if(m_objProgressCircle.isShowing() && m_objProgressCircle !=null)
+                        m_objProgressCircle.dismiss();
+                    ProgressFlag = false;
+                    Toast.makeText(mContext,getResources().getString(R.string.CompleteMessage),Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+    //endregion
+
+    public void Test()
+    {
+//        String lang = "eng";
+        image = BitmapFactory.decodeResource(getResources(), R.drawable.sampledata);
+        mDataPath = getFilesDir()+ "/tesseract/";
+
+//        checkFile2(new File(mDataPath + "tessdata/"),lang);
+
+        String lang = "";
+        for (String Language : mLanguageList) {
+            checkFile(new File(mDataPath + "tessdata/"), Language);
+            lang += Language + "+";
+        }
+        lang = lang.substring(0,lang.length()-1);
+        m_Tess = new TessBaseAPI();
+        m_Tess.init(mDataPath, lang);
+    }
+    private void copyFiles2(String lang) {
+        try {
+            //location we want the file to be at
+            String filepath = mDataPath + "/tessdata/"+lang+".traineddata";
+
+            //get access to AssetManager
+            AssetManager assetManager = getAssets();
+
+            //open byte streams for reading/writing
+            InputStream instream = assetManager.open("tessdata/"+lang+".traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            //copy the file to the location specified by filepath
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkFile2(File dir,String lang) {
+        //directory does not exist, but we can successfully create it
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFiles2(lang);
+        }
+        //The directory exists, but there is no data file in it
+        if(dir.exists()) {
+            String datafilepath = mDataPath+ "/tessdata/"+lang+".traineddata";
+            File datafile = new File(datafilepath);
+            if (!datafile.exists()) {
+                copyFiles2(lang);
+            }
+        }
+    }
+    //Process an Image
+    public void processImage() {
+        OCRThread ocrThread = new OCRThread(image);
+        ocrThread.setDaemon(true);
+        ocrThread.start();
     }
 }

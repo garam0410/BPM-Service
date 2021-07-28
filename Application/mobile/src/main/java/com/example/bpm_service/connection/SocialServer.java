0a339d22dd58addb2bd50289736eb72f914c8a95
Garam Kim <0410garam@naver.com>
2021-07-28 17:54:05 +0900

    소셜 서비스 통신 매서드 구현

---
+++ Application/mobile/src/main/java/com/example/bpm_service/connection/SocialServer.java
@@ -0,0 +1,58 @@
+package com.example.bpm_service.connection;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
+public class SocialServer {
+    private String url = "";
+    private String data = "";
+
+    public SocialServer(String IP){
+        this.url = IP;
+    }
+
+    //댓글 추가
+    public String insertComment(String cid, String commentData){
+        try {
+            url += "8082/comments/"+cid;
+            return new PostTask().execute(url,commentData).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return "ExecutionError";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "InterruptedError";
+        }
+    }
+
+    // 댓글 삭제
+    public String deleteComment(String cid){
+        try {
+            url += "8082/comments/delete/"+cid;
+            return new GetTask().execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return "ExecutionError";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "InterruptedError";
+        }
+    }
+
+    // 댓글 목록 조회
+    public JSONArray selectCommentList(String title){
+        try {
+            url += "8082/comments/"+title;
+            data = new GetTask().execute(url).get();
+            JSONObject json = new JSONObject(data);
+            JSONArray getData = (JSONArray) json.get("commentList");
+            return getData;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}

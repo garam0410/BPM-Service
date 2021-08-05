40ec0e398e512f093017e76c5a00c3e08f8cb968
Garam Kim <0410garam@naver.com>
2021-08-05 13:57:43 +0900

    로그아웃 기능 추가

--- Application/mobile/src/main/java/com/example/bpm_service/uinfo/MyPageActivity.java
+++ Application/mobile/src/main/java/com/example/bpm_service/uinfo/MyPageActivity.java
@@ -6,17 +6,57 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.bpm_service.MainActivity;
 import com.example.bpm_service.R;
+import com.example.bpm_service.login.LoginActivity;
 
 public class MyPageActivity extends Fragment {
+
+    private Button logout;
+    private boolean SAVE_LOGIN_DATA;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
         View view = inflater.inflate(R.layout.activity_mypage, container, false);
+
+        SharedPreferences appData = this.getActivity().getSharedPreferences("appData", Activity.MODE_PRIVATE);
+        logout = (Button) view.findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(getContext()).setTitle("로그아웃").setMessage("로그아웃 하시겠습니까?")
+                        .setPositiveButton("로그아웃", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent i = new Intent(getContext(), LoginActivity.class);
+                                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                                SAVE_LOGIN_DATA = false;
+                                i.putExtra("SAVE_LOGIN_DATA", false);
+                                startActivity(i);
+                                SharedPreferences.Editor editor = appData.edit();
+                                editor.clear();
+                                editor.commit();
+                                getActivity().finish();
+                            }
+                        }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                        }).show();
+            }
+        });
 
         return view;
     }
